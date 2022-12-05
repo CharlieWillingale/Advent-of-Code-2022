f74c2dc7c5e69bb6407614dc9df7061991aeb008
CharlieWillingale <48034544+CharlieWillingale@users.noreply.github.com>
2022-12-05 12:52:41 +0000

    Day 5 part 1 solution added

---
+++ day_5/d5_sol.py
@@ -0,0 +1,71 @@
+import re
+
+# Load input for the games
+# with open('./input.txt','r') as f:
+with open('./day_5/input.txt','r') as f:
+
+    input = f.readlines()
+
+#
+# Part 1 solution
+#
+
+def part1():
+    # find number of stacks in input
+    num_stacks = 0
+    for line in input:
+        try:
+            if line.strip()[0] == '1':
+                num_stacks = len(line.strip().replace(' ',''))
+                break
+
+        except IndexError:
+            pass
+
+    # create list of stacks for each stack
+    stacks = []
+
+    for i in range(0, num_stacks):
+        stacks.append([])
+
+    # Fill stckas w/ initial configuration
+    for line in input:
+        
+    
+        if len(line.strip()) > 1 and line.strip()[0] != '1':
+            line = re.sub(r'\s{3}',r' - ',line)
+            for item in range(0,len(line.split())):
+                if line.split()[item] != '-':
+                    stacks[item].append(line.split()[item].strip('[').strip(']'))
+
+        else:
+            break
+
+    for stack in stacks:
+        stack.reverse()
+
+    #  Complete actions   
+    for line in input:
+        
+        if len(line.split()) > 0 and line.split()[0] == 'move':
+            instructions = line.split()
+            num_move = int(instructions[1])
+            move_from = int(instructions[3]) - 1
+            move_to = int(instructions[5]) - 1
+
+            for moves in range(0, num_move):
+
+                top_item = stacks[move_from].pop(-1)
+                stacks[move_to].append(top_item)
+
+    for stack in stacks:
+        print(stack.pop(),end='')
+
+
+#
+# Part 2 solution
+#
+
+
+
+part1()
